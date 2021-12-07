81e8e73fad4f1ddf4e65ef504bcebbe150c2419c
Aleksey Sobol <yourbrandbook@gmail.com>
2021-12-07 09:02:15 +0300

    v 1.0.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,51 @@
 import '../css/style.css'
 import './plugins';
-import locations from "./store/locations";
-locations.init().then(res => {
+import locations from './store/locations';
+import formUI from './views/form';
+
+document.addEventListener('DOMContentLoaded', () => {
+initApp();
+
+const form = formUI.form;
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    onFormSubmit();
+    
+})
+
+//events
+
+
+//handlers
+     async function initApp (){
+         await locations.init();
+         formUI.setAutocomplete(locations.shortCitiesList);
+     }
+
+     async function onFormSubmit(){
+         //collection data
+         const origin = locations.getCityCodeByKey(formUI.originValue);
+         const destination = locations.getCityCodeByKey(formUI.originValue);
+         const depart_date = formUI.departDateValue;
+         const return_date = formUI.returnDateValue;
+
+         console.log(origin,destination,depart_date,return_date )
+         await locations.fetchTickets({
+             origin,
+             destination,
+             depart_date,
+             return_date
+         })
+     }
+})
+
+/* locations.init().then(res => {
     console.log(res) 
     console.log(locations)
     console.log(locations.getCitiesCountryCode('PE'))
 });
-
+ */
 /* import api from './service/apiService';
 api.countries().then(res => console.log(res))
 api.cities().then(res => console.log(res)) */
--- src/js/service/apiService.js
+++ src/js/service/apiService.js
@@ -32,6 +32,16 @@ async cities (){
     }
 }
 async prices (params){
+    try {
+        const response = await axios.get(`${this.url}/prices/cheap`, {
+            params,
+        });
+        return response.data;
+       
+    } catch (err) {
+    console.log(err) 
+    return Promise.reject(err)
+    }
     
 }
 
